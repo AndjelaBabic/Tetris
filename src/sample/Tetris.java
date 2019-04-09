4e459140521fa070037c595f4b5b26efba83494e
Andjela Babic <andjelasbabic@gmail.com>
2019-04-09 23:49:36 +0200

    Instead of timer.utl use TimeLine from javafx; Implemented move down; Removing rows

--- src/sample/Tetris.java
+++ src/sample/Tetris.java
@@ -1,19 +1,22 @@
 package sample;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-import java.text.Normalizer;
-import java.util.Arrays;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 
 public class Tetris extends Application {
 
@@ -59,7 +62,7 @@ public class Tetris extends Application {
         // Creating first block
         Form a = nextObj;
         group.getChildren().addAll(a.a, a.b, a.c, a.d);
-        //moveOnKeyPress(a);
+        moveOnKeyPressed(a);
         object = a;
         nextObj = Controller.makeRect();
         primaryStage.setScene(scene);
@@ -67,38 +70,34 @@ public class Tetris extends Application {
         primaryStage.show();
 
         // Timer
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                if (object.a.getY() == 0 || object.b.getY() == 0 || object.c.getY() == 0 || object.d.getY() == 0) {
-                    time++;
-                } else {
-                    time = 0;
-                }
-                // for the 2 seconds the block is on the top
-                if (time == 2) {
-                    Text over = new Text("Game over");
-                    over.setFill(Color.RED);
-                    over.setStyle("-fx-font: 70 arial;");
-                    over.setY(250);
-                    over.setX(10);
-                    group.getChildren().add(over);
-                    game = false;
-                }
-                if (time == 15) {
-                    System.exit(0);
-                }
-
-                if (game) {
-                    moveDown(object);
-                    scoreText.setText("Score: " + score);
-                    lines.setText("Lines: " + linesNo);
-                }
+        Timeline timer = new Timeline(new KeyFrame(Duration.seconds(1), ev -> {
+            if (object.a.getY() == 0 || object.b.getY() == 0 || object.c.getY() == 0 || object.d.getY() == 0) {
+                time++;
+            } else {
+                time = 0;
+            }
+            // for the 2 seconds the block is on the top
+            if (time == 2) {
+                Text over = new Text("Game over");
+                over.setFill(Color.RED);
+                over.setStyle("-fx-font: 70 arial;");
+                over.setY(250);
+                over.setX(10);
+                group.getChildren().add(over);
+                game = false;
+            }
+            if (time == 15) {
+                System.exit(0);
             }
 
-        };
-        timer.schedule(task, 0, 300);
+            if (game) {
+                moveDown(object);
+                scoreText.setText("Score: " + score);
+                lines.setText("Lines: " + linesNo);
+            }
+        }));
+        timer.setCycleCount(Animation.INDEFINITE);
+        timer.play();
     }
 
     private void moveOnKeyPressed(Form form) {
@@ -122,5 +121,166 @@ public class Tetris extends Application {
                 }
             }
         });
+    }
+
+    private void moveTurn(Form form) {
+        int f = form.form;
+        Rectangle a = form.a;
+        Rectangle b = form.b;
+        Rectangle c = form.c;
+        Rectangle d = form.d;
+        switch (form.getName()) {
+            case "j":
+                if (f == 1 && checkBoundries(a, 1, -1)) {
+
+                }
+                break;
+            case "l":
+                break;
+            case "o":
+                break;
+            case "s":
+                break;
+            case "t":
+                break;
+            case "z":
+                break;
+            case "i":
+                break;
+        }
+
+    }
+
+    private boolean checkBoundries(Rectangle rect, int x, int y) {
+        boolean xok = false;
+        boolean yok = false;
+
+        if (rect.getX() + MOVE * x + SIZE <= XMAX && rect.getX() + MOVE * x >= 0) {
+            xok = true;
+        }
+        if (rect.getY() + MOVE * y + SIZE <= YMAX || rect.getY() + MOVE * y >= 0) {
+            yok = true;
+        }
+        return xok && yok && MESH[(int) (rect.getX() / SIZE + x)][(int) (rect.getY() / SIZE + y)] == 0;
+
+
+    }
+
+    private void moveRight(Rectangle rect) {
+        rect.setX(rect.getX() + MOVE);
+    }
+
+    private void moveLeft(Rectangle rect) {
+        rect.setX(rect.getX() - MOVE);
+    }
+
+    private void moveUp(Rectangle rect) {
+        rect.setY(rect.getY() - MOVE);
+    }
+
+    private void moveDown(Form form) {
+        // if the block touched other, or if it reached till the end, stop it, set mesh field to 1, remove rows if necessary, get next object
+        if (form.a.getY() + MOVE == YMAX || form.b.getY() + MOVE == YMAX || form.c.getY() + MOVE == YMAX || form.d.getY() + MOVE == YMAX
+                || touchingOtherBlock(form)) {
+            MESH[(int) (form.a.getX() / SIZE)][(int) (form.a.getY() / SIZE)] = 1;
+            MESH[(int) (form.b.getX() / SIZE)][(int) (form.b.getY() / SIZE)] = 1;
+            MESH[(int) (form.c.getX() / SIZE)][(int) (form.c.getY() / SIZE)] = 1;
+            MESH[(int) (form.d.getX() / SIZE)][(int) (form.d.getY() / SIZE)] = 1;
+
+            removeRows(group);
+            Form a = nextObj;
+            nextObj = Controller.makeRect();
+            object = a;
+            group.getChildren().addAll(a.a, a.b, a.c, a.d);
+            moveOnKeyPressed(a);
+        }
+        // moving the block down
+        if (form.a.getY() + MOVE < YMAX && form.b.getY() + MOVE < YMAX && form.c.getY() + MOVE < YMAX && form.d.getY() + MOVE < YMAX
+                && !touchingOtherBlock(form)) {
+            form.a.setY(form.a.getY() + MOVE);
+            form.b.setY(form.b.getY() + MOVE);
+            form.c.setY(form.c.getY() + MOVE);
+            form.d.setY(form.d.getY() + MOVE);
+        }
+
+    }
+
+    private void removeRows(Pane group) {
+
+        List<Integer> rowsToRemoveIndex = new ArrayList<>();
+        List<Node> blocks = new ArrayList<>();
+        List<Node> newBlocks = new ArrayList<>();
+
+        for (int y = 0; y < YMAX / SIZE; y++) {
+            int numberInRow = 0;
+            for (int x = 0; x < XMAX / SIZE; x++) {
+                if (MESH[x][y] == 1) {
+                    numberInRow++;
+                }
+            }
+            if (numberInRow == XMAX / SIZE) {
+                rowsToRemoveIndex.add(y);
+            }
+        }
+
+        score += rowsToRemoveIndex.size() * 50;
+        linesNo += rowsToRemoveIndex.size();
+    // TODO Try to improve this part 
+        while(rowsToRemoveIndex.size() > 0) {
+            // get all blocks
+            for (Node node : group.getChildren()) {
+                if (node instanceof Rectangle) {
+                    blocks.add(node);
+                }
+            }
+            // for current row to delete remove nodes
+            for (Node node : blocks){
+                Rectangle rect = (Rectangle) node;
+                if(rect.getY() / SIZE == rowsToRemoveIndex.get(0)){
+                    MESH[(int) rect.getX()/SIZE][(int) rect.getY()/SIZE] = 0;
+                    group.getChildren().remove(rect);
+                } else{
+                    newBlocks.add(node);
+                }
+            }
+
+            for (Node node : newBlocks){
+                Rectangle rect = (Rectangle) node;
+                if(rect.getY()/SIZE < rowsToRemoveIndex.get(0)){
+                    MESH[(int) rect.getX()/SIZE][(int) rect.getY()/SIZE] = 0;
+                    rect.setY(rect.getY() + SIZE);
+                }
+            }
+
+            blocks.clear();
+            newBlocks.clear();
+            rowsToRemoveIndex.remove(0);
+
+            for (Node node : group.getChildren()){
+                if(node instanceof Rectangle) blocks.add(node);
+            }
+
+            for (Node node : blocks){
+                Rectangle rect = (Rectangle) node;
+                try {
+                    MESH[(int) rect.getX()/SIZE][(int) rect.getY()/SIZE] = 1;
+                } catch (ArrayIndexOutOfBoundsException ex){
+
+                }
+            }
+            blocks.clear();
+
+        }
+
+        score += rowsToRemoveIndex.size() * 50;
+        linesNo += rowsToRemoveIndex.size();
+    }
+
+
+    private boolean touchingOtherBlock(Form form) {
+        return MESH[(int) (form.a.getX() / SIZE)][(int) (form.a.getY() / SIZE + 1)] == 1 ||
+                MESH[(int) (form.b.getX() / SIZE)][(int) (form.b.getY() / SIZE + 1)] == 1 ||
+                MESH[(int) (form.c.getX() / SIZE)][(int) (form.c.getY() / SIZE + 1)] == 1 ||
+                MESH[(int) (form.d.getX() / SIZE)][(int) (form.d.getY() / SIZE + 1)] == 1;
     }
 }
